b59dd04fad125c3dc4b80885b69c055191398281
zacinthepark <zacinthepark@gmail.com>
2021-11-21 18:19:43 +0900

    Initial Project Version

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by zac on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
