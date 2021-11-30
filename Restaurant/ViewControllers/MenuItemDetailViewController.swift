12e28e17ec957d926b7fe1e0c2e6c070c1037231
zacinthepark <zacinthepark@gmail.com>
2021-11-30 19:23:12 +0900

    Set up MenuItemDetailVC

--- Restaurant/ViewControllers/MenuItemDetailViewController.swift
+++ Restaurant/ViewControllers/MenuItemDetailViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     //Since the detail screen will never be presented without a MenuItem object in place, you can define the property as an implicitly unwrapped optional
     var menuItem: MenuItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addToOrderButton.layer.cornerRadius = 5.0
+        updateUI()
     }
     
 
@@ -28,5 +34,21 @@ class MenuItemDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    @IBAction func addToOrderButtonTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+    }
+    
+}
 
+extension MenuItemDetailViewController {
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
+    }
 }
