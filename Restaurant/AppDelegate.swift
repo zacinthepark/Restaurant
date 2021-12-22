9bb78d1ddc9f1b6077326a57ff304c1070677e24
zacinthepark <zacinthepark@gmail.com>
2021-12-23 02:13:33 +0900

    Save and load order

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -9,10 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        MenuController.shared.loadOrder()
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        MenuController.shared.saveOrder()
     }
 
     // MARK: UISceneSession Lifecycle
--- Restaurant/ModelControllers/MenuController.swift
+++ Restaurant/ModelControllers/MenuController.swift
@@ -89,3 +89,24 @@ class MenuController {
         task.resume()
     }
 }
+
+extension MenuController {
+    
+    func loadOrder() {
+        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let orderFileURL = documentsDirectoryURL.appendingPathComponent("order").appendingPathExtension("json")
+        
+        guard let data = try? Data(contentsOf: orderFileURL) else { return }
+        order = (try? JSONDecoder().decode(Order.self, from: data)) ?? Order(menuItems: [])
+    }
+    
+    func saveOrder() {
+        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let orderFileURL = documentsDirectoryURL.appendingPathComponent("order").appendingPathExtension("json")
+        
+        if let data = try? JSONEncoder().encode(order) {
+            try? data.write(to: orderFileURL)
+        }
+    }
+    
+}
